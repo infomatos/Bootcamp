e0dd710b771d74a4a86ce5415ec7a81ae2a5a117
Elias Matos <ematos@id.uff.br>
2024-07-28 21:05:32 -0300

    API Spring Boot

---
+++ api-spring-boot-2024/src/main/java/me/dio/ApiSpringBoot2024Application.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSpringBoot2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSpringBoot2024Application.class, args);
+	}
+
+}
